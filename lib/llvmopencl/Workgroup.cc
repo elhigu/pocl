79ffb8a9a201b723749a5b7ffda257cc47cec6bf
Mikael Lepistö <mikael.lepisto@vincit.fi>
2014-08-21 08:20:14 +0300

    Added dirty copy-paste snprintf fix.

--- lib/llvmopencl/Workgroup.cc
+++ lib/llvmopencl/Workgroup.cc
@@ -65,6 +65,45 @@
 
 #include "pocl.h"
 
+// TODO: refactor this...
+#ifdef _MSC_VER
+#include <io.h>
+#include <stdlib.h>
+#include <direct.h>
+#define MKDIR(x) mkdir(x)
+#define strtok_r strtok_s
+
+// http://stackoverflow.com/questions/2915672/snprintf-and-visual-studio-2010
+#include <stdarg.h>
+#define snprintf c99_snprintf
+
+inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
+{
+	int count = -1;
+
+	if (size != 0)
+		count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+	if (count == -1)
+		count = _vscprintf(format, ap);
+
+	return count;
+}
+
+inline int c99_snprintf(char* str, size_t size, const char* format, ...)
+{
+	int count;
+	va_list ap;
+
+	va_start(ap, format);
+	count = c99_vsnprintf(str, size, format, ap);
+	va_end(ap);
+
+	return count;
+}
+
+#endif
+
+
 #define STRING_LENGTH 32
 
 using namespace std;
